630bef3e740a72299bb50eac7b10fb0508ce3ec9
mkreive <mkreive@gmail.com>
2022-03-24 16:57:32 +0200

    coding

--- script.js
+++ script.js
@@ -3,17 +3,34 @@
 // Selectors
 
 const ratingButtons = document.querySelectorAll(".review__button");
+const submitButton = document.querySelector(".review__button-submit");
+const ratingMessage = document.querySelector(".thankyou__rating");
+const reviewCard = document.querySelector(".review");
+const thankyouCard = document.querySelector(".thankyou");
 
 // Code
 
+const active = "active";
+ratingMessage.innerText = `No rating selected`;
+let rating;
+
 ratingButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-        let btnActive = false;
-        if (!button.classList.contains("active") && btnActive === false) {
-            button.classList.add("active");
-            return (btnActive = true);
-        } else {
-            button.classList.remove("active");
-        }
+    button.addEventListener("click", function (e) {
+        const clickedRating = e.target;
+
+        ratingButtons.forEach((button) => {
+            if (button == clickedRating) return;
+            button.classList.remove(active);
+        });
+
+        clickedRating.classList.toggle(active);
+        rating = clickedRating.innerText;
     });
+});
+
+submitButton.addEventListener("click", function () {
+    // reviewCard.classList.add("hidden");
+    thankyouCard.classList.remove("hidden");
+    ratingMessage.innerText = `You selected ${rating} out of 5`;
+    reviewCard.classList.add("flipped");
 });
