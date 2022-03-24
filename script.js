062807a1a21a80e89adaf2ed89e94a94941b7384
mkreive <mkreive@gmail.com>
2022-03-24 16:03:35 +0200

    code

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// Selectors
+
+const ratingButtons = document.querySelectorAll(".review__button");
+
+// Code
+
+ratingButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+        let btnActive = false;
+        if (!button.classList.contains("active") && btnActive === false) {
+            button.classList.add("active");
+            return (btnActive = true);
+        } else {
+            button.classList.remove("active");
+        }
+    });
+});
